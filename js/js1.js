5f7b26ccb3fa26b04682dc220280e785a7fd76ed
k-elven <2279356591@qq.com>
2024-09-28 20:56:46 +0800

    progress3

--- js/js1.js
+++ js/js1.js
@@ -1,2 +1,45 @@
-const img = document.querySelectorAll('.liders img')
-console.log(img)
+const imgs = document.querySelector('.sliders img')
+const spans = document.querySelectorAll('.carousel-progress span')
+const progress = document.querySelector('.carousel-progress')
+const sliders = document.querySelector('.sliders')
+const slider = document.querySelector('.slider')
+   
+progress.addEventListener('click',function(e){
+    if(e.target.tagName === 'SPAN'){
+        let num = +e.target.dataset.id                
+        sliders.innerHTML = `<img src="./picture/slider${num}.jpeg" alt="">`
+        //平滑播放效果
+        sliders.style.transform = `translateX(-${(num-1) * 100}%)`;
+        //移除类
+        progress.querySelector('.active').classList.remove('active')
+        //添加类 
+        e.target.classList.add('active')    
+    }
+})
+//轮播图定时器
+let index = 0
+let timeId=setInterval(function(){
+    spans.forEach(span => span.classList.remove('active'));  
+    sliders.innerHTML = `<img src="./picture/slider${index + 1}.jpeg" alt="" class="slider-image">`
+    spans[index].classList.add('active')
+    index++
+    if(index >= 3){
+        index = 0
+    }
+},2000)
+//鼠标经过和离开
+slider.addEventListener('mouseenter',() =>{
+    clearInterval(timeId)
+})
+slider.addEventListener('mouseleave',() =>{
+    clearInterval(timeId)
+    timeId=setInterval(function(){
+    spans.forEach(span => span.classList.remove('active'));  
+    sliders.innerHTML = `<img src="./picture/slider${index + 1}.jpeg" alt="" class="slider-image">`
+    spans[index].classList.add('active')
+    index++
+    if(index >= 3){
+        index = 0
+    }
+},2000)
+})
